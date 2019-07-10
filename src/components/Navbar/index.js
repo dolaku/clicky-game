a4366d65254210387a4ee758fce469a2ed856faf
dolaku <dolaku@gmail.com>
2019-07-10 18:42:26 -0400

    Initial navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+//sets up the reusable Navbar component
+import React, { Component } from "react";
+import './style.css'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white sticky-top d-flex justify-content-around">
+                <h2>Clicky Game</h2>
+                <h2>Score: </h2>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
