c66b9a73ab4a83a9accd32c0a6a58b9c16748778
Donna Kuang <dolaku@gmail.com>
2019-07-12 16:52:49 -0400

    Updates

---
+++ precache-manifest.19b375f2db982d3dfdf8947d0a80c217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cb097efb1ab200be05cae9771d79ce",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "5ec0d7719645462353ae",
+    "url": "/clicky-game/static/css/main.56d852d0.chunk.css"
+  },
+  {
+    "revision": "d82ae84299c75f2741a5",
+    "url": "/clicky-game/static/js/2.d4fd61bb.chunk.js"
+  },
+  {
+    "revision": "5ec0d7719645462353ae",
+    "url": "/clicky-game/static/js/main.70b57bb5.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
