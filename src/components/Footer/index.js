7aaf94c750290bb8f3c25b5cce3f791d477c99e8
dolaku <dolaku@gmail.com>
2019-07-12 22:07:57 -0400

    Added hover styles on cards

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './style.css'
 const Footer = () => (
     <footer className="pt-3 text-white bg-dark">
         <div className="d-flex flex-column justify-content-center align-items-center">
-            <h5>Donna Kuang</h5>
+            <h5><a href="https://dolaku.github.io/" target="_blank" rel="noopener noreferrer">Donna Kuang</a></h5>
             <a className="link-email" href="mailto:dolaku@gmail.com">dolaku@gmail.com</a>
             <a className="display-4 link-email" href="https://github.com/dolaku" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
             <p className="small">&copy; Copyright 2019. All Rights Reserved.</p>
